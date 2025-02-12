94e4ce6a48f34745f2f28268ad63907e668d1202
chaewon kim <cheawon0319@gmail.com>
2025-02-11 20:05:24 -0800

    add request to backend for testing in the same state

--- cypress/e2e/note_app.cy.js
+++ cypress/e2e/note_app.cy.js
@@ -1,5 +1,12 @@
 describe('Note app', function() {
   beforeEach(function() {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Chaewon Kim',
+      username: 'chaewon',
+      password: 'thisispw'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:5173')
   })
 
@@ -19,5 +26,21 @@ describe('Note app', function() {
     cy.get('#login-button').click()
 
     cy.contains('Chaewon Kim logged in')
+  })
+
+  describe('when logged in', function() {
+    beforeEach(function() {
+      cy.contains('log in').click()
+      cy.get('#username').type('chaewon')
+      cy.get('#password').type('thisispw')
+      cy.get('#login-button').click()
+    })
+
+    it('a new note can be created', function() {
+      cy.contains('new note').click()
+      cy.get('input').type('a note created by cypress')
+      cy.contains('save').click()
+      cy.contains('a note created by cypress')
+    })
   })
 })
