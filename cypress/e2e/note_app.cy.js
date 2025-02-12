23a7bb0151f0e9f9f78b257a563359c2cef92453
chaewon kim <cheawon0319@gmail.com>
2025-02-11 19:45:45 -0800

    testing for logging in

--- cypress/e2e/note_app.cy.js
+++ cypress/e2e/note_app.cy.js
@@ -1,7 +1,23 @@
 describe('Note app', function() {
-  it('front page can be opened', function() {
+  beforeEach(function() {
     cy.visit('http://localhost:5173')
+  })
+
+  it('front page can be opened', function() {
     cy.contains('Notes')
     cy.contains('Made by React')
+  })
+
+  it('login form can be opened', function() {
+    cy.contains('log in').click()
+  })
+
+  it('user can log in', function () {
+    cy.contains('log in').click()
+    cy.get('#username').type('chaewon')
+    cy.get('#password').type('thisispw')
+    cy.get('#login-button').click()
+
+    cy.contains('Chaewon Kim logged in')
   })
 })
